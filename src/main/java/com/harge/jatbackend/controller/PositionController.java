61947ac83f838c9da4c56f8950e52abfbcebd9a2
Matt Hargenrader <hargenrader.matt@gmail.com>
2021-04-13 10:15:49 -0400

    Added custom query functionality for positions

--- src/main/java/com/harge/jatbackend/controller/PositionController.java
+++ src/main/java/com/harge/jatbackend/controller/PositionController.java
@@ -2,12 +2,14 @@ package com.harge.jatbackend.controller;
 
 import java.util.List;
 
+import com.harge.jatbackend.exception.ResourceNotFoundException;
 import com.harge.jatbackend.model.Position;
 import com.harge.jatbackend.repository.PositionRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,31 @@ public class PositionController
     public List<Position> getAllPositions()
     {
         return positionRepository.findAll();
+    }
+
+    // get distinct LCATs
+    @GetMapping("/positions/lcats")
+    public List<Object> getDistinctLCATDescriptions() 
+    {
+        return positionRepository.findLCATDescriptions();
+    }
+
+    // get LCAT Levels by LCAT Description
+    @GetMapping("/positions/{lcatDescription}")
+    public List<Object> getLCATLevelsByLCATDescription(@PathVariable String lcatDescription)
+    {
+        List<Object> lcatLevels = null;
+        try 
+        {
+            lcatLevels = positionRepository.findLCATLevels(lcatDescription);
+        }
+        
+        catch (ResourceNotFoundException ex) 
+        {
+            ex.getMessage();
+        }
+
+        return lcatLevels;
     }
 
     // add Position
